a88dc2e9d61862737e8241b87f96ed2635a8bb62
lashdid19 <lashdid19@gmail.com>
2022-09-09 10:37:31 +0700

    bruh

--- lib/loadProjects.ts
+++ lib/loadProjects.ts
@@ -1,7 +1,10 @@
 export async function loadProjects() {
   // Call an external API endpoint to get posts
-  const serverUrl = process.env.NEXT_PUBLIC_URL
-  const res = await fetch(serverUrl + "/api/projects");
+  const serverUrl: string = process.env.NEXT_PUBLIC_URL!
+  const env: string = process.env.NODE_ENV!
+  const clean = "https://" + serverUrl.split("%2F")[2].toString().split("%3F")[0];
+  const url = env === "development" ? serverUrl : clean
+  const res = await fetch(url + "/api/projects");
   const data = await res.json();
   return data;
 }
